46b443757a60b89b576e6129367023c87834e16c
Varmo <101868197+varmoh@users.noreply.github.com>
2025-09-22 13:26:10 +0300

    Update config.py

--- file-processing/app/core/config.py
+++ file-processing/app/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     aws_secret_access_key: str
     aws_region: str = 'us-east-1'
     s3_bucket_name: str
+    s3_endpoint_url: str
     s3_presigned_url_expiration: int = 3600
 
     class Config:
